4aa856fd56f38198c70574f7ffb06ec62b9f1f80
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-06-14 08:12:55 +0100

    add import statements to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
-import {favicons} from "favicons";
-import {app} from "./js/app.js";
-import {route} from "./js/route.js";
+import WeatherService from './js/weather-service.js';
