d92724fecd9beb56b80ea7db7bd74126e69223f4
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-06-14 08:24:12 +0100

    add function clearFields to main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import WeatherService from './js/weather-service.js';
+
+
+
+function clearFields() {
+    $('#location').val("");
+    $('.showErrors').text("");
+    $('.showHumidity').text("");
+    $('.showTemp').text("");
+    $('.visibility').text("");
+    $('.pressure').text("");
+    $('.windSpeed').text("");
+    $('.clouds').text("");
+  }
