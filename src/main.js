d09a5ad4c94c1162c5971ea1339ae93f5791e594
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-06-14 08:25:33 +0100

    add .ready method to main.js

--- src/main.js
+++ src/main.js
@@ -16,3 +16,22 @@ function clearFields() {
     $('.windSpeed').text("");
     $('.clouds').text("");
   }
+
+  $(document).ready(function() {
+    $('#weatherLocation').click(function() {
+      let city = $('#location').val();
+      clearFields();
+      let promise = WeatherService.getWeather(city);
+      promise.then(function(response) {
+        const body = JSON.parse(response);
+        $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+        $('.visibility').text(`The visibility in meters is ${body.visibility} meters.`);
+        $('.pressure').text(`The pressure in ${city} is ${body.main.pressure} hPa.`);
+        $('.windSpeed').text(`The wind speed in ${city} is ${body.wind.speed} km/hr.`);
+        $('.clouds').text(`The cloud situation in ${city} is ${body.clouds.all} oktas or eights.`);
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error}`);
+      });
+    });
+  });
