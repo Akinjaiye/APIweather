07914c94188efedb6f006399eee2cf7e93916d4d
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-06-14 07:57:01 +0100

    add class container to index.html

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import './css/style.css';
 import {favicons} from "favicons";
+import {app} from "./js/app.js";
+import {route} from "./js/route.js";
